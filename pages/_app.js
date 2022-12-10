66e0bc753703c8cb864d74067040c1c6ab3be182
Santiago <Martnez Bota>
2022-12-10 19:52:37 -0300

    check how to use css vars inside a css modules (it must import vars.css into _app.js and access normally in css module)

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import '../styles/colors.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/skills/index.jsx
+++ pages/skills/index.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import styles from './skills.module.css';
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div className={styles.prueba}>Skills</div>
   )
 }
 
