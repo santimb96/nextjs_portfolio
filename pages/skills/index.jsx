cfad6f6f8794ef972ce32ea5f1497e15966e5c6c
Santiago <Martnez Bota>
2022-12-12 00:08:06 -0300

    fetch skill data and add jest unit testing

--- pages/skills/index.jsx
+++ pages/skills/index.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import styles from './Skills.module.css';
-
+import React, { useEffect, useState } from "react";
+import getApiData from "../../src/utils/apiData";
+import styles from "./Skills.module.css";
 const Skills = () => {
-  return (
-    <div className={styles.prueba}>Skills</div>
-  )
-}
+  const [skills, setSkills] = useState({});
+  const [error, setError] = useState(null);
 
-export default Skills
+  const getData = () => getApiData("/api/skillAPI");
+
+  useEffect(() => {
+    getData()
+      .then((data) => setSkills(data))
+      .catch((err) => setError(err));
+  }, []);
+  return (
+    <div className={styles.prueba}>
+      {error ? error?.message : skills?.frontend}
+    </div>
+  );
+};
+
+export default Skills;
---
+++ src/utils/apiData.js
@@ -0,0 +1,11 @@
+// import fetch from 'node-fetch';
+
+const getApiData = (endpoint) => new Promise((resolve, reject) => {
+  
+  fetch(endpoint)
+    .then(response => response.json())
+    .then(data =>resolve(data))
+    .catch(error => reject({message:'Error al obtener los datos', error}));
+});
+
+export default getApiData;
