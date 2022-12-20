5fed40ee151c26fd9ec48de79f517089a28607bf
Santiago <Martnez Bota>
2022-12-20 19:54:23 -0300

    working with clamp() function (css)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
+import { MdMail } from 'react-icons/md'
 import styles from './Footer.module.css'
 
 const Footer = ({ name, socialMedia }) => {
+  const ICONS = {
+    github: <BsGithub />,
+    linkedin: <BsLinkedin />,
+    mail: <MdMail />
+  }
+
   return (
     <div className={styles.footerCard}>
       <div className={styles.title}>
@@ -10,7 +18,9 @@ const Footer = ({ name, socialMedia }) => {
       </div>
       <div className={styles.socialMedia}>
         {socialMedia?.map((platform) => (
-          <p>{platform?.name}</p>
+          <a className={styles.socialLink} key={platform?.name} target='_blank' href={platform?.link}>
+            {ICONS[(platform?.name).toLowerCase()]}
+          </a>
         ))}
       </div>
     </div>
