5979f1004cdf875182bfbce515f85153cfb183cd
Santiago <Martnez Bota>
2023-01-16 16:31:04 -0300

    do experience component

---
+++ src/components/CollectionComponents/Experience/ExperienceCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from './ExperienceCard.module.css'
+const ExperienceCard = () => {
+  return <div>ExperienceCard</div>
+}
+
+export default ExperienceCard
---
+++ src/components/CollectionComponents/Experience/ExperienceList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ExperienceCard from './ExperienceCard'
+import styles from './ExperienceList.module.css'
+
+const ExperienceList = () => {
+  return <div>ExperienceList</div>
+}
+
+export default ExperienceList
