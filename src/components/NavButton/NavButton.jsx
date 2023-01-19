53cc6fb24e396e93894c00845ea8ad5785034034
Santiago <Martnez Bota>
2023-01-19 19:12:25 -0300

    doing query params for every page

--- src/components/NavButton/NavButton.jsx
+++ src/components/NavButton/NavButton.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import styles from './NavButton.module.css'
 import { COLLECTIONS_DIC } from '../../utils/translationDictionary'
 
 const NavButton = ({ name, duration, setPage, isCurrentRoute }) => {
+  const { push } = useRouter()
   return (
     <>
       <button
-        onClick={() => setPage(name)}
+        onClick={() => push(`?page=${name}`)}
         className={`${styles.navButton} ${isCurrentRoute ? styles.currentRoute : ''}`}
         style={{ animationDelay: duration / 10 + 's' }}
       >
