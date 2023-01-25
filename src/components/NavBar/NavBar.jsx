3ff669e29c8a53e0cb44550270359c7af134d804
Santiago <Martnez Bota>
2023-01-25 19:31:41 -0300

    add transition and new navbar color when the user scroll down 50 px

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import NavButton from '../NavButton/NavButton'
 import { PAGES } from '../../utils/router'
 import { RxHamburgerMenu } from 'react-icons/rx'
@@ -7,6 +7,7 @@ import useMediaQuery from '@mui/material/useMediaQuery'
 import styles from './NavBar.module.css'
 
 const NavBar = ({ page, setShow }) => {
+  const navRef = useRef(0)
   const [open, setOpen] = useState(false)
   const match = useMediaQuery('(min-width: 768px)')
   const handleOpen = () => {
@@ -20,6 +21,14 @@ const NavBar = ({ page, setShow }) => {
   }
 
   useEffect(() => {
+    const navElem = navRef.current
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        navElem.classList.add(styles.backgroundDarker)
+      } else {
+        navElem.classList.remove(styles.backgroundDarker)
+      }
+    })
     setOpen(false)
     setShow(true)
   }, [page])
@@ -31,7 +40,7 @@ const NavBar = ({ page, setShow }) => {
           {open ? <IoCloseOutline className={styles.buttonType} /> : <RxHamburgerMenu className={styles.buttonType} />}
         </button>
       </div>
-      <nav className={open || match ? styles.navContainer : styles.displayNone}>
+      <nav ref={navRef} className={open || match ? styles.navContainer : styles.displayNone}>
         {PAGES?.map((name, idx) => (
           <NavButton key={name} name={name?.toLowerCase()} duration={idx} isCurrentRoute={page === name?.toLowerCase()} />
         ))}
